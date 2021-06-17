123bff9d5daa65ebc3fb614d4b74d0d1d812429b
Paul Coral <paul.coral@epfl.ch>
2021-06-17 18:38:21 +0200

    fix tests

--- src/test/scala/ll1compiletime/CompileTimeLL1Test.scala
+++ src/test/scala/ll1compiletime/CompileTimeLL1Test.scala
@@ -5,35 +5,6 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 import scala.quoted._
 
-enum MyToken {
-    // Integer Token
-    case IntT(i:Int) extends MyToken
-    // separator
-    case SepT extends MyToken
-    // parenthese token
-    case ParT(open:Boolean) extends MyToken 
-}
-enum MyKind {
-    case IntK extends MyKind
-    case SepK extends MyKind
-    case ParK(open:Boolean) extends MyKind
-}
-
-given ToExpr[MyKind] with {
-    def apply(k:MyKind)(using Quotes) = k match {
-        case MyKind.IntK => '{MyKind.IntK}
-        case MyKind.SepK => '{MyKind.SepK}
-        case MyKind.ParK(o) => '{MyKind.ParK(${Expr(o)})}
-    }
-}
-
-def getMyKind(t:MyToken):MyKind = t match {
-    case MyToken.IntT(_) => MyKind.IntK
-    case MyToken.SepT => MyKind.SepK
-    case MyToken.ParT(o) => MyKind.ParK(o)
-}
-
-
 
 class CompileTimeLL1Test extends AnyFlatSpec {
     import MyToken._
--- src/test/scala/ll1compiletime/SyntaxDefTesting.scala
+++ src/test/scala/ll1compiletime/SyntaxDefTesting.scala
@@ -3,11 +3,40 @@ package ll1compiletime
 import scala.quoted._
 import scala.language.implicitConversions
 
-import MyKind._
-import MyToken._
 
+
+enum MyToken {
+    // Integer Token
+    case IntT(i:Int) extends MyToken
+    // separator
+    case SepT extends MyToken
+    // parenthese token
+    case ParT(open:Boolean) extends MyToken 
+}
+enum MyKind {
+    case IntK extends MyKind
+    case SepK extends MyKind
+    case ParK(open:Boolean) extends MyKind
+}
+
+given ToExpr[MyKind] with {
+    def apply(k:MyKind)(using Quotes) = k match {
+        case MyKind.IntK => '{MyKind.IntK}
+        case MyKind.SepK => '{MyKind.SepK}
+        case MyKind.ParK(o) => '{MyKind.ParK(${Expr(o)})}
+    }
+}
+
+def getMyKind(t:MyToken):MyKind = t match {
+    case MyToken.IntT(_) => MyKind.IntK
+    case MyToken.SepT => MyKind.SepK
+    case MyToken.ParT(o) => MyKind.ParK(o)
+}
 
 object P1 {
+    import MyKind._
+    import MyToken._
+
     private inline def parsingTable = ${init}
 
     private def init(using Quotes) = Expr(buildParsingTable(SyntaxDefTesting))
@@ -43,6 +72,9 @@ object P1 {
 }
 
 object P2 {
+    import MyKind._
+    import MyToken._
+
     private inline def parsingTable = ${init}
 
     private def init(using Quotes) = Expr(buildParsingTable(SyntaxDefTesting))
